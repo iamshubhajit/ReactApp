f6be3da494f826ca83db533d305fc7e0b41058b0
iamshubhajit <sbhblore1@gmail.com>
2021-08-10 02:12:50 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   return (
     <div>
       <h2>lets get started</h2>
       <p>This is also visible</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
