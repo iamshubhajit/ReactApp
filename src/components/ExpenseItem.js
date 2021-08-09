5083fa13d6b28546a61ae06c0564974606ec3a40
iamshubhajit <sbhblore1@gmail.com>
2021-08-10 02:55:27 +0530

    commit

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,9 +2,16 @@ import './ExpenseItem.css'
 
 function ExpenseItem(props) {
 
+    const month = props.date.toLocaleString('en-US', { month: 'long' });
+    const year = props.date.toLocaleString('en-US', { year: 'numeric' });
+    const day = props.date.toLocaleString('en-US', { day: 'numeric' });
     return (
         <div className="expense-item">
-            <div>{props.date}</div>
+            <div>
+                <div>{day}</div>
+                <div>{month}</div>
+                <div>{year}</div>
+            </div>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
