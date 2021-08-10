b058f3ad8904c1c32cfc08c2edbf40e98e3d9831
iamshubhajit <sbhblore1@gmail.com>
2021-08-10 20:46:24 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import ExpenseItem from "./components/ExpenseItem";
+import React from 'react';
 
-function App() {
+import Expenses from './components/Expenses/Expenses';
 
+const App = () => {
   const expenses = [
     {
       id: 'e1',
@@ -24,15 +25,17 @@ function App() {
     },
   ];
 
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
 
   return (
     <div>
-      <h2>lets get started</h2>
-      <p>This is also visible</p>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
